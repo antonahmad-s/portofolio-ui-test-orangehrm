47fee79a7761e2f792f7c279cfa658db6a41042c
Anton Ahmad <71451871+antonahmad-s@users.noreply.github.com>
2026-09-20 21:56:11 +0700

    feat: add src/utils/testData.js

---
+++ src/utils/testData.js
@@ -0,0 +1,4 @@
+module.exports={
+  validUser:{username:process.env.USERNAME || "Admin",password:process.env.PASSWORD || "admin123"},
+  invalidUser:{username:"InvalidUser",password:"InvalidPassword"}
+};
