57ece8bb3bbaaca015f87f5bb9a660fa14cef997
Anton Ahmad <71451871+antonahmad-s@users.noreply.github.com>
2026-09-20 21:56:50 +0700

    feat: add test/leave/leave.spec.js

---
+++ test/leave/leave.spec.js
@@ -0,0 +1,18 @@
+const {expect}=require("chai");
+const {createDriver}=require("../../src/utils/driver");
+const LoginPage=require("../../src/pages/LoginPage");
+const LeavePage=require("../../src/pages/LeavePage");
+const config=require("../../config/env");
+
+describe("Leave",function(){
+  let driver,leave;
+  beforeEach(async function(){
+    driver=await createDriver();
+    const login=new LoginPage(driver);
+    leave=new LeavePage(driver);
+    await login.open();
+    await login.login(config.username,config.password);
+  });
+  afterEach(async function(){if(driver) await driver.quit();});
+  it("should open Leave module",async function(){await leave.open(); expect(await leave.isDisplayed()).to.equal(true);});
+});
