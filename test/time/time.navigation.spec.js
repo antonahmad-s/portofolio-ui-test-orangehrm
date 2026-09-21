5777baf431147bdf93452c6522ef6f9b26ec9ffd
Anton Ahmad <71451871+antonahmad-s@users.noreply.github.com>
2026-09-21 09:28:34 +0700

    test: expand test/time/time.navigation.spec.js

---
+++ test/time/time.navigation.spec.js
@@ -0,0 +1,25 @@
+const {expect}=require("chai");
+const {createDriver}=require("../../src/utils/driver");
+const LoginPage=require("../../src/pages/LoginPage");
+const config=require("../../config/env");
+
+describe("Time - navigation",function(){
+  let driver;
+  beforeEach(async function(){
+    driver=await createDriver();
+    const login=new LoginPage(driver);
+    await login.open();
+    await login.login(config.username,config.password);
+    await driver.get(config.baseUrl+"/web/index.php/time/viewEmployeeTimesheet");
+  });
+  afterEach(async function(){if(driver) await driver.quit();});
+
+  it("should load Time module",async function(){
+    expect(await driver.getCurrentUrl()).to.contain("/time/");
+  });
+
+  it("should display timesheet content",async function(){
+    const body=(await driver.findElement({css:"body"})).getText();
+    expect((await body).toLowerCase()).to.contain("timesheet");
+  });
+});
