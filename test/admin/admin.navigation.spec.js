f3b33161e81ca88dc0c834d996f3f90774cd0639
Anton Ahmad <71451871+antonahmad-s@users.noreply.github.com>
2026-09-21 09:28:30 +0700

    test: expand test/admin/admin.navigation.spec.js

---
+++ test/admin/admin.navigation.spec.js
@@ -0,0 +1,25 @@
+const {expect}=require("chai");
+const {createDriver}=require("../../src/utils/driver");
+const LoginPage=require("../../src/pages/LoginPage");
+const config=require("../../config/env");
+
+describe("Admin - navigation",function(){
+  let driver;
+  beforeEach(async function(){
+    driver=await createDriver();
+    const login=new LoginPage(driver);
+    await login.open();
+    await login.login(config.username,config.password);
+    await driver.get(config.baseUrl+"/web/index.php/admin/viewAdminModule");
+  });
+  afterEach(async function(){if(driver) await driver.quit();});
+
+  it("should load Admin module",async function(){
+    expect(await driver.getCurrentUrl()).to.contain("/admin/");
+  });
+
+  it("should display User Management content",async function(){
+    const body=(await driver.findElement({css:"body"})).getText();
+    expect((await body).toLowerCase()).to.contain("user management");
+  });
+});
